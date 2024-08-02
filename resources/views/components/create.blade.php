90088b787d4055a7201ca68b667c726d38223c33
RizalAnas00 <belangsumerlang@gmail.com>
2024-08-02 14:19:37 +0700

    fetch

---
+++ resources/views/components/create.blade.php
@@ -0,0 +1,5 @@
+<button class="flex items-center justify-center h-20 w-20 p-2 bg-blue-700 rounded-full ml-2 hover:bg-blue-400">
+    <svg class="w-12 h-12 flex-it" xlmns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <path d="M24 13.7143H13.7143V24H10.2857V13.7143H0V10.2857H10.2857V0H13.7143V10.2857H24V13.7143Z" fill="white"/>
+    </svg>
+</button>
